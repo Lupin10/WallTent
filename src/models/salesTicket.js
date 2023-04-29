f8dc987d270285ae6d80c726b4edcc7f2d2396cd
Santiago Pulido <madeinhell15@gmail.com>
2023-04-29 00:05:00 -0500

    Creación schema salesTicket
    Se crea dentro de la carpeta models el archivo salesTicket.js en el que se encuentra el schema de salesTicket.

---
+++ src/models/salesTicket.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const salesTicketSchema = mongoose.Shema({
+    ticketId:{
+        type: Number,
+        required: true
+    },
+    clientId:{
+        type: Number,
+        required: true
+    },
+    quantity:{
+        type: Number,
+        required: true
+    },
+    totalValue:{
+        type: Number,
+        required: true
+    },
+    salesTicket:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Sales_Ticket',
+        required: true
+    }
+});
+
+module.exports = mongoose.model("Sales_Ticket", salesTicketSchema)
