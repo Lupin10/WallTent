e63d739cfe341da2a4607981a10fd49ae2677752
Lupin <santimw18@gmail.com>
2023-04-29 18:47:54 -0500

    Creación esquema play y routes/plays
    Se añade 1 archivo .js correspondiente al esquema de "play" en la carpeta de modules. 
    Además, se agrega un nuevo archivo .js en la carpeta "routes" que contendrá el crud de este esquema.

---
+++ src/routes/plays.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+const playSchema = require("../models/play");
+
+//New Play
+router.post("/plays", (req, res) => {
+    const play = playSchema (req.body);
+    snack
+        .save()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
