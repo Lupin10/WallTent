1c0554020e33ab1cc67cf06f6fb823dc9e4411d6
Lupin <santimw18@gmail.com>
2023-04-30 19:50:00 -0500

    Creación de Crud para employee y student
    Se añadieron las funciones de CRUD para los esquemas de employee y student.

--- src/routes/employees.js
+++ src/routes/employees.js
@@ -10,4 +10,35 @@ router.post("/employees", (req, res) => {
         .then((data) => res.json(data))
         .catch((error) => res.json({ message: error }));
 });
+//Consult document of the Employee collection
+router.get("/employees", (req, res)=>{
+    employeeSchema.find()
+    .then((data) => res.json(data))
+    .catch((error)=>res.json({message: error}));
+});
+//Consult a employee by ID
+router.get("/employees/:id", (req, res)=>{
+    const{id} = req.params;
+    employeeSchema.findById(id)
+    .then((data) => res.json(data))
+    .catch((error)=>res.json({message: error}));
+});
+//Edit employee by ID
+router.put("/employees/:id", (req, res)=>{
+    const{id} = req.params;
+    const{name, id_card, birthday, member, student} = req.body; 
+    employeeSchema.updateOne({_id:id}, {
+        $set: {name, id_card, birthday, member, student}
+    })
+    .then((data) => res.json(data))
+    .catch((error)=>res.json({message: error}))
+});
+//Delete employee by ID
+router.delete("/employees/:id", (req, res)=>{
+    const{id} = req.params;
+    employeeSchema
+    .findOneAndDelete({_id:id})
+    .then((data) => res.json(data))
+    .catch((error)=>res.json({message: error}));
+});
 module.exports = router;
--- src/routes/students.js
+++ src/routes/students.js
@@ -10,4 +10,35 @@ router.post("/students", (req, res) => {
         .then((data) => res.json(data))
         .catch((error) => res.json({ message: error }));
 });
+//Consult document of the Student collection
+router.get("/students", (req, res)=>{
+    studentSchema.find()
+    .then((data) => res.json(data))
+    .catch((error)=>res.json({message: error}));
+});
+//Consult a student by ID
+router.get("/students/:id", (req, res)=>{
+    const{id} = req.params;
+    studentSchema.findById(id)
+    .then((data) => res.json(data))
+    .catch((error)=>res.json({message: error}));
+});
+//Edit student by ID
+router.put("/students/:id", (req, res)=>{
+    const{id} = req.params;
+    const{university} = req.body; 
+    studentSchema.updateOne({_id:id}, {
+        $set: {university}
+    })
+    .then((data) => res.json(data))
+    .catch((error)=>res.json({message: error}))
+});
+//Delete student by ID
+router.delete("/students/:id", (req, res)=>{
+    const{id} = req.params;
+    studentSchema
+    .findOneAndDelete({_id:id})
+    .then((data) => res.json(data))
+    .catch((error)=>res.json({message: error}));
+});
 module.exports = router;
